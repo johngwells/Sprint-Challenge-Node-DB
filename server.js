e676f5fbed2aafdb9a7c9e3c1f928fd69684ac8e
John Wells <molekewl2017@gmail.com>
2019-11-08 12:13:01 -0800

    add morgan helmet and connect routes

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const projectsRouter = require('./routes/projects-router');
+const resourcesRouter = require('./routes/resources-router');
 
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'))
 
 server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
 
 server.get('/', (req, res) => {
-  res.send('Server is up & running');
+  res.send('Doing the impossible possible!');
 });
 
 module.exports = server;
