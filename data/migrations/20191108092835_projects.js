c3aed5caa2fbe18eb0b76a4af7666b936fa8f923
John Wells <molekewl2017@gmail.com>
2019-11-08 10:30:42 -0800

    fixed promise all

--- data/migrations/20191108092835_projects.js
+++ data/migrations/20191108092835_projects.js
@@ -1,20 +1,20 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('projects', tbl => {
       tbl.increments();
-      tbl.integer('id').unique();
+      // tbl.integer('id').unique();
       tbl.string('name').notNullable();
       tbl.string('description');
       tbl.boolean('boolean', false);
     }),
     knex.schema.createTable('resources', tbl => {
-      tbl.integer('id').unique();
-      tbl.string('name').notNullable();
+      // tbl.integer('id').unique();
+      tbl.string('name').unique().notNullable();
       tbl.string('description');
     }),
     knex.schema.createTable('tasks', tbl => {
-      tbl.interger('id').unique();
+      // tbl.integer('id').unique();
       tbl.string('description').notNullable();
       tbl.string('notes');
       tbl.boolean('boolean', false);
@@ -22,7 +22,7 @@ exports.up = function(knex, Promise) {
   ])
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return Promise.all([
     knex.schema.dropTableIfExists('projects'),
     knex.schema.dropTableIfExists('resources'),
